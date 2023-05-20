598db8936f1e5f0bcab9c9549c5fd062d0996fbf
SyrianSam <sami.k92@gmail.com>
2023-05-20 19:17:59 +0200

    Refactor

--- ima.h
+++ ima.h
@@ -12,18 +12,12 @@ struct Image {
     GLubyte *data;
 };
 
-struct Vono{
-
-    
+struct Vono{    
 	GLubyte valvR;
 	GLubyte valvG;
 	GLubyte valvB;
 	int position; //stores memory offset for each point
-	
-
-
 };
-
 
 typedef struct Vono Vono;
 typedef struct Image Image;
@@ -34,17 +28,3 @@ void imagesave_PPM(char *filename, Image *image);
 void imagesave_VONO(char *filename, Vono *vono, Image *image);
 int decompression(char * filename, Image *image);
 void gris_uniforme (Image *);
-/*
-void floattoint3(float, float, float, int *, int *, int *);
-void inttofloat3(int, int, int, float *, float *, float*);
-void rgbtohls(float, float, float, float *, float *, float *);
-void hlstorgb(float, float, float, float *, float *, float *);
-void modifclr(GLubyte *, GLubyte *, int);
-void dlr27(Image *), dlr54(Image *), dlr27fs(Image *);
-*/
-
-/*void untrait (Image *), untraitOK (Image *);
-void moserr (Image *), mosprox(Image *);
-Image * try2  (Image *);
-Image * toclut  (Image *);
-*/
